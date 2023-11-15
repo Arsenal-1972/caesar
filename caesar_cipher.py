7fad19d0b43c1b2e408789bbb863ecf4b2f1f6dd
Arsenal-1972 <149561978+Arsenal-1972@users.noreply.github.com>
2023-11-15 10:15:58 +0200

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,21 +1,21 @@
-a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 # Funkcija a definē alfabetu
-def e(t, o): # Funkcija e definē šifru
+def encription(text, rotation): # Funkcija e definē šifru
   r = ""
-  for c in t:
-    if (a.find(c) == -1):
+  for c in text: 
+    if (abc.find(c) == -1):
       r += c
     else:
-      r += (a[(a.find(c) + o) % len(a)])
+      r += (abc[(abc.find(c) + rotation) % len(abc)])
   return r
 
-def d(t, o): # Funkcija d definē atšifrēšanas funkciju
+def decription(text, rotation): # Funkcija d definē atšifrēšanas funkciju
   r = ""
-  for c in t:
-    if (a.find(c) == -1):
+  for c in text: 
+    if (abc.find(c) == -1): 
       r += c
     else:
-      r += (a[(a.find(c) - o) % len(a)])
+      r += (abc[(abc.find(c) - rotation) % len(abc)])
   return r
 
 w = """
@@ -28,11 +28,11 @@ mode = int(input(w))
 if mode == 1: # Definē šifra šifrēšanas funkciju
   text = input("Enter the text: ")
   rotation = int(input("Enter the rotation: "))
-  print("Encrypted: " + e(text, rotation))
+  print("Encrypted: " + encription(text, rotation))
 elif mode == 2: # Definē atšifrēšanas funkciju
   text = input("Enter the text: ")
   rotation = int(input("Enter the rotation: "))
-  print("Decrypted: " + d(text, rotation))
+  print("Decrypted: " + decription(text, rotation))
 elif mode == 3: # Definē bruteforce funkciju
   print("Bruteforcing...")
   print("But I don't know how to do it, sorry ¯\_(ツ)_/¯")
